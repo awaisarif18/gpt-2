33e2107129fb865028a4290ff6290fdc4e985aae
Muhammad Awais Arif <sp22-bcs-113@cuilahore.edu.pk>
2026-08-10 17:25:32 +0500

    added loop for 5 sequence prompts, and used tiktoken and fixed missing head_size error

--- train_gpt2.py
+++ train_gpt2.py
@@ -2,7 +2,8 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-
+import tiktoken
+import math
 
 class CausalSelfAttention(nn.Module):
 
@@ -16,6 +17,7 @@ class CausalSelfAttention(nn.Module):
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
+        self.head_size = config.n_embd // config.n_head
         # not really a 'bias', more of a mask to ensure that attention is only applied to the left in the input sequence, but following the OpenAI/HF naming through
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
                                      .view(1, 1, config.block_size, config.block_size))
@@ -164,5 +166,51 @@ class GPT(nn.Module):
         return model
 
 # -----------------------------------------------------------------------------
+
+num_return_sequences = 5
+max_length = 30
+
+
 model = GPT.from_pretrained('gpt2')
-print("didn't crash yay!")
+model.eval()
+model.to('cuda')
+
+
+# prefix tokens
+
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
+x = tokens.to('cuda')
+
+
+# generate! right now x is (B, T) where B = 5, T = 8
+# set the seed to 42
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    # forward the model to get the logits
+    with torch.no_grad():
+        logits = model(x) # (B, T, vocab_size)
+        # take the logits at the last position
+        logits = logits[:, -1, :] # (B, vocab_size)
+        # get the probabilities
+        probs = F.softmax(logits, dim=-1)
+        # do top-k sampling of 50 (huggingface pipeline default)
+        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        # select a token from the top-k probabilities
+        # note: multinomial does not demand the input to sum to 1
+        ix = torch.multinomial(topk_probs, 1) # (B, 1)
+        # gather the corresponding indices
+        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+        # append to the sequence
+        x = torch.cat((x, xcol), dim=1)
+
+# print the generated text
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
+
